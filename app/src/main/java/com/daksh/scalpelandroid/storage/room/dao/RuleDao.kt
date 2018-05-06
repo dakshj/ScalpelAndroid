a6fa6a615aa570052488adeb4e42485875c5b229
Daksh Jotwani <dakshjay@gmail.com>
2018-05-06 15:37:27 -0400

    Move static rules to RuleDao.kt

--- app/src/main/java/com/daksh/scalpelandroid/storage/room/dao/RuleDao.kt
+++ app/src/main/java/com/daksh/scalpelandroid/storage/room/dao/RuleDao.kt
@@ -2,6 +2,7 @@ package com.daksh.scalpelandroid.storage.room.dao
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Query
+import android.arch.persistence.room.Transaction
 import com.daksh.scalpelandroid.storage.room.entity.Rule
 import io.reactivex.Flowable
 
@@ -21,4 +22,38 @@ abstract class RuleDao : BaseDao<Rule> {
 
     @Query("DELETE FROM Rule")
     abstract fun clear()
+
+    @Transaction
+    open fun addStaticRules() {
+        if (!this.isEmpty()) {
+            this.clear()
+        }
+
+        this.insert(
+                Rule(
+                        extension = "jpg",
+                        maxBytesAmount = 200000000,
+                        header = "\\xff\\xd8",
+                        footer = "\\xff\\xd9"
+                ),
+                Rule(
+                        extension = "gif",
+                        maxBytesAmount = 5000000,
+                        header = "\\x47\\x49\\x46\\x38\\x37\\x61",
+                        footer = "\\x00\\x3b"
+                ),
+                Rule(
+                        extension = "gif",
+                        maxBytesAmount = 5000000,
+                        header = "\\x47\\x49\\x46\\x38\\x39\\x61",
+                        footer = "\\x00\\x00\\x3b"
+                ),
+                Rule(
+                        extension = "html",
+                        maxBytesAmount = 50000,
+                        header = "<html>",
+                        footer = "</html>"
+                )
+        )
+    }
 }
