a7887923b51ecd844d8b2d0eae64b6f816e00a9c
Daksh Jotwani <dakshjay@gmail.com>
2018-05-06 15:31:35 -0400

    Add snackbar functions to BaseActivity.kt

--- app/src/main/java/com/daksh/scalpelandroid/base/BaseActivity.kt
+++ app/src/main/java/com/daksh/scalpelandroid/base/BaseActivity.kt
@@ -1,6 +1,20 @@
 package com.daksh.scalpelandroid.base
 
+import android.support.design.widget.Snackbar
+import android.view.View
 import dagger.android.support.DaggerAppCompatActivity
 
 abstract class BaseActivity : DaggerAppCompatActivity() {
+
+    protected fun shortSnackbar(view: View, resId: Int) =
+            Snackbar.make(view, resId, Snackbar.LENGTH_SHORT).show()
+
+    protected fun shortSnackbar(view: View, text: String) =
+            Snackbar.make(view, text, Snackbar.LENGTH_SHORT).show()
+
+    protected fun longSnackbar(view: View, resId: Int) =
+            Snackbar.make(view, resId, Snackbar.LENGTH_LONG).show()
+
+    protected fun longSnackbar(view: View, text: String) =
+            Snackbar.make(view, text, Snackbar.LENGTH_LONG).show()
 }
